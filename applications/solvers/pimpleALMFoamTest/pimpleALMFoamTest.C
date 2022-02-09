04572dbe0c9c77c2b46781b1c327dfb05b3671c3
Tobias <64459693+TobiasLong@users.noreply.github.com>
2022-02-09 18:17:41 +0000

    Update pimpleALMFoamTest.C

--- applications/solvers/pimpleALMFoamTest/pimpleALMFoamTest.C
+++ applications/solvers/pimpleALMFoamTest/pimpleALMFoamTest.C
@@ -37,7 +37,7 @@ Description
 #include "singlePhaseTransportModel.H"
 #include "kinematicMomentumTransportModel.H"
 #include "pimpleControl.H"
-#include "CorrectPhi.H"
+#include "correctPhi.H"
 #include "fvOptions.H"
 #include "localEulerDdtScheme.H"
 #include "fvcSmooth.H"
